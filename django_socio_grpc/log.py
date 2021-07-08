96ccd60c3095d04c3f0d56b11e810448db9b4134
fbouazza <felix.bouazza@socotec.com>
2021-07-08 10:05:55 +0200

    add default logging config and logging action

--- django_socio_grpc/log.py
+++ django_socio_grpc/log.py
@@ -2,70 +2,33 @@
 logging utils
 """
 import logging
-import logging.config
 
-from django.core.management.color import color_style
-from django.utils.module_loading import import_string
+from django_socio_grpc.settings import grpc_settings
 
 DEFAULT_LOGGING = {
     "version": 1,
     "disable_existing_loggers": False,
+    "filters": {"require_debug_false": {"()": "django.utils.log.RequireDebugFalse"}},
     "formatters": {
-        "django_socio_grpc": {
-            "()": "grpc_framework.utils.log.ServerFormatter",
-            "format": "[{server_time}] {message} {resp_code} {resp_time}",
+        "socio_grpc_format": {
+            "format": "{asctime}:{levelno}:{name}:{pathname}:{funcName}:{lineno:d}:{levelname}:{message}",
+            "datefmt": "%Y-%m-%d %H:%M:%S",
             "style": "{",
-        }
+        },
     },
     "handlers": {
-        "django_socio_grpc": {
-            "level": "INFO",
-            "class": "logging.StreamHandler",
-            "formatter": "django_socio_grpc",
+        "socio_grpc_handler": {
+            "level": "DEBUG",
+            "class": "django_socio_grpc.log.GRPCHandler",
+            "formatter": "socio_grpc_format",
         },
     },
     "loggers": {
-        "grpc": {
-            "handlers": ["console"],
-            "level": "INFO",
-        },
-        "django_socio_grpc": {
-            "handlers": ["console", "django_socio_grpc"],
-            "level": "INFO",
-        },
+        "socio_grpc_logger": {"handlers": ["socio_grpc_handler"], "propagate": False},
     },
 }
 
-
-def configure_logging(logging_config, logging_settings):
-    if logging_config:
-        logging_config_func = import_string(logging_config)
-
-        logging.config.dictConfig(DEFAULT_LOGGING)
-
-        if logging_settings:
-            logging_config_func(logging_settings)
-
-
-class ServerFormatter(logging.Formatter):
-    def __init__(self, *args, **kwargs):
-        self.style = color_style()
-        super().__init__(*args, **kwargs)
-
-    def format(self, record):
-        msg = record.msg
-        code = getattr(record, "resp_code", None)
-        if code:
-            if code == "success":
-                msg = self.style.HTTP_SUCCESS(msg)
-            else:
-                msg = self.style.HTTP_SERVER_ERROR(msg)
-
-        if self.uses_server_time() and not hasattr(record, "server_time"):
-            record.server_time = self.formatTime(record, self.datefmt)
-
-        record.msg = msg
-        return super().format(record)
-
-    def uses_server_time(self):
-        return self._fmt.find("{server_time}") >= 0
+class GRPCHandler(logging.Handler):
+    def emit(self, record):
+        self.format(record)
+        grpc_settings.LOGGING_ACTION(record)

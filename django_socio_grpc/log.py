e856f23adc15fa4b5e3e48c3f1ee512a42a46cfb
fbouazza <felix.bouazza@socotec.com>
2021-07-08 16:20:15 +0200

    Rewrite configure logging function in log.py

--- django_socio_grpc/log.py
+++ django_socio_grpc/log.py
@@ -2,31 +2,42 @@
 logging utils
 """
 import logging
+import logging.config
 
 from django_socio_grpc.settings import grpc_settings
+from django.utils.module_loading import import_string
 
 DEFAULT_LOGGING = {
     "version": 1,
     "disable_existing_loggers": False,
     "filters": {"require_debug_false": {"()": "django.utils.log.RequireDebugFalse"}},
     "formatters": {
-        "socio_grpc_format": {
+        "django_socio_grpc_format": {
             "format": "{asctime}:{levelno}:{name}:{pathname}:{funcName}:{lineno:d}:{levelname}:{message}",
             "datefmt": "%Y-%m-%d %H:%M:%S",
             "style": "{",
         },
     },
     "handlers": {
-        "socio_grpc_handler": {
+        "django_socio_grpc_handler": {
             "level": "DEBUG",
             "class": "django_socio_grpc.log.GRPCHandler",
-            "formatter": "socio_grpc_format",
+            "formatter": "django_socio_grpc_format",
         },
     },
     "loggers": {
-        "socio_grpc_logger": {"handlers": ["socio_grpc_handler"], "propagate": False},
+        "django_socio_grpc": {"handlers": ["django_socio_grpc_handler"], "propagate": False},
     },
 }
+
+def configure_logging(logging_config, logging_settings):
+    if logging_config:
+        logging_config_func = import_string(logging_config)
+
+        logging.config.dictConfig(DEFAULT_LOGGING)
+
+        if logging_settings:
+            logging_config_func(logging_settings)
 
 class GRPCHandler(logging.Handler):
     def emit(self, record):

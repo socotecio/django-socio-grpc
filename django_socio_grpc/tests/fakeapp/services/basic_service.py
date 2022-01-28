fc8fc4def606af1d5b86a7e961b16c20c735c5e6
Adrien Montagu <adrienmontagu@gmail.com>
2022-01-28 14:14:36 +0100

    remove test error in fake app

--- django_socio_grpc/tests/fakeapp/services/basic_service.py
+++ django_socio_grpc/tests/fakeapp/services/basic_service.py
@@ -29,10 +29,3 @@ class BasicService(generics.GenericService):
     )
     async def TestEmptyMethod(self, request, context):
         print("TestEmptyMethod")
-
-    @grpc_action(
-        request={"name": "test", "type": "string"},
-        response="google.protobuf.Empty",
-    )
-    async def TestError(self, request, context):
-        print("TestError")

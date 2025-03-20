41f95cad359cfc5320b252a4c8ef8ba7506e3894
Adrien Montagu <adrienmontagu@gmail.com>
2025-03-20 12:22:25 +0100

    update lock to match runtime with generated version

--- django_socio_grpc/tests/fakeapp/grpc/fakeapp_pb2_grpc.py
+++ django_socio_grpc/tests/fakeapp/grpc/fakeapp_pb2_grpc.py
@@ -6,7 +6,7 @@ import warnings
 from django_socio_grpc.tests.fakeapp.grpc import fakeapp_pb2 as django__socio__grpc_dot_tests_dot_fakeapp_dot_grpc_dot_fakeapp__pb2
 from google.protobuf import empty_pb2 as google_dot_protobuf_dot_empty__pb2
 
-GRPC_GENERATED_VERSION = '1.70.0'
+GRPC_GENERATED_VERSION = '1.71.0'
 GRPC_VERSION = grpc.__version__
 _version_not_supported = False
 
